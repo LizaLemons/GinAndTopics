5cacaa8761c853a6b03a446fbf222974d5262f25
LizaLemons <liza.h.ramo@gmail.com>
2014-09-03 14:19:51 -0400

    changes

--- public/javascripts/views/searchView.js
+++ public/javascripts/views/searchView.js
@@ -1,6 +1,9 @@
 
+// el: '#search'
+
+
 // input
 // API call with search terms ==> retrieves URLs
 // perform load API call
 
-// listener on the search button in the CollectionView 
+// listener on the search button in the CollectionView
